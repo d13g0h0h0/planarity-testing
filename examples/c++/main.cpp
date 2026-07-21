8ec518ef969a1c8b843089200c7cc845f9f90a97
d13g0h0h0 <diego.jamroz@gmail.com>
2026-07-21 13:06:03 +0200

    Implement Hopcrof-Tarjan bicomponent algorithm

--- examples/c++/main.cpp
+++ examples/c++/main.cpp
@@ -3,7 +3,8 @@
 
 int main()
 {
-    Graph g(std::set<std::pair<size_t, size_t>>{{1, 2}, {2, 3}, {3, 5}});
-    std::cout << "Edge count: " << g.edges() << std::endl;
+    Graph g(std::set<Graph::Edge>{{1, 2}, {2, 3}, {1, 3}, {3, 5}});
+    std::cout << "Edge count: " << g.get_edge_count() << std::endl;
     std::cout << "Connected component count: " << g.get_connected_components().size() << std::endl;
+    std::cout << "Biconnected component count: " << g.get_biconnected_components().size() << std::endl;
 }
